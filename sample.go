e6bb47cdb95262d000c25d264245a9e6e6917314
Hum9183 <hum9183@gmail.com>
2023-11-24 22:58:26 +0900

    fmt.Print()

--- sample.go
+++ sample.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world")
+	num := 123
+	fmt.Print("Hello, world", num)
+	fmt.Println("Hello, world", num)
+	fmt.Printf("Hello, world%d", num)
+
+	const pi = 3.14
 }
